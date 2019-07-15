0fa5997099c4f1789dbf93a165880c284d49e0db
1keshav1 <32426423+1keshav1@users.noreply.github.com>
2019-07-15 18:23:32 +0530

    Storage and test folder

---
+++ storage/framework/views/66abd541fc68d1e57dad53bff67e9abf684aca6f.php
@@ -0,0 +1,31 @@
+<?php $__env->startSection('content'); ?>
+
+<h2>Contact Us <strong></strong></h2>
+
+<?php if(count($persions)): ?>
+<ul>
+<?php $__currentLoopData = $persions; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $persion): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+
+<li>Name: <?php echo e($persion); ?></li>
+
+<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+</ul>
+
+<?php endif; ?>
+
+<?php $__env->stopSection(); ?>
+
+
+
+
+
+
+<?php $__env->startSection('footer'); ?>
+<script>
+//alert('Hi');
+</script>
+
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\wamp64\www\laraveld.in\resources\views/pages/contact.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/7f828b71c4d096aebbb5c9de8a0a7b6cdcc5ade6.php
@@ -0,0 +1,8 @@
+<?php $__env->startSection('content'); ?>
+
+<h2>Post Page <strong><?php echo e($id); ?></strong></h2>
+
+<?php $__env->stopSection(); ?>
+
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\wamp64\www\laraveld.in\resources\views/pages/post_view.blade.php ENDPATH**/ ?>
