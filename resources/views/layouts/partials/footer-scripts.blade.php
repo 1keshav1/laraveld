b6d5833c1bd26e7adbe2f9929e14a31460545455
1keshav1 <32426423+1keshav1@users.noreply.github.com>
2019-07-29 17:56:45 +0530

    View

---
+++ resources/views/layouts/partials/footer-scripts.blade.php
@@ -0,0 +1,6 @@
+<!-- Bootstrap core JavaScript
+================================================= -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
---
+++ resources/views/layouts/partials/head.blade.php
@@ -0,0 +1,13 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>Album example for Bootstrap</title>
+
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+
+<!-- Custom styles for this template -->
+<link href="/css/album.css" rel="stylesheet">
